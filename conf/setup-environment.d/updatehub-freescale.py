996fafa9e53ceb48aea8a7abccbeb30b81569bd2
Fabio Berton <fabio.berton@ossystems.com.br>
2017-08-02 11:40:43 -0300

    Initial Layer Structure
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

---
+++ conf/setup-environment.d/updatehub-freescale.py
@@ -0,0 +1,9 @@
+def __after_init_updatehub_freescale():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                        'meta-updatehub-freescale',
+                    ]])
+
+run_after_init(__after_init_updatehub_freescale)
